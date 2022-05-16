8b30beb6d0b90077b76b7f2c4805fc732d835276
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-16 15:19:35 -0600

    Test: add more expectations to get all merchants request test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,7 +10,15 @@ RSpec.describe "Merchants API Requests" do
       
       merchants = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
+      expect(merchants).to have_key(:data)
 
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a String
+
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a String
+      end 
     end 
 
   end 
