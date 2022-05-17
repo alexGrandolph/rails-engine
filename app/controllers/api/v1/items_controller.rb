92202fcf7b975da89e982ebdfddd43819ec7c23a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-16 19:25:47 -0600

    Feat: add route, and show action for returning one item when given id

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    
     render json: ItemSerializer.new(Item.all)
+  end
+  
+  def show
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item)
   end
   
 
