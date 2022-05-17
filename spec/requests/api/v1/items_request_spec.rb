7b6cb2d8bd3e2fc71248f3f3c25c3edef536ce0d
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-16 19:26:57 -0600

    Test: add more expectations to returning one item by id test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -40,7 +40,21 @@ RSpec.describe "items API Requests" do
       get "/api/v1/items/#{id}"
 
       expect(response).to be_successful
+      item = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_a(String)
 
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a Float
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an Integer     
     end 
 
 
