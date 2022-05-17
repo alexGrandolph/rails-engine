32377643ea4432f868db0ff55cb36a4a5a6714bf
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-16 19:22:13 -0600

    Test: add more expectations to item index test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -14,6 +14,29 @@ RSpec.describe "items API Requests" do
       items = JSON.parse(response.body,symbolize_names: true)[:data]
       
       expect(items).to be_a(Array)
+      items.each do |item|
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes][:name]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a Float
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an Integer
+        
+      end 
     end 
+
+    
+
+
+
+
   end 
 end 
