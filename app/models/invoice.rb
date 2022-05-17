ca87c1c991b2519aacd63a72386c107930b4973e
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-17 13:52:39 -0600

    Feat: add missing relationships and dependent destroy to invoice, items, invoice items

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,6 @@
 class Invoice < ApplicationRecord
-  has_many :invoice_items
-  has_many :items, through: :invoice_items
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items, dependent: :destroy
   belongs_to :customer
   belongs_to :merchant
 end
