79840ed4e14e046998ff6e0e96a057e21f68b47b
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-18 10:51:51 -0600

    TEst and test for Merchant.find_one_by_search_term method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,6 +8,18 @@ RSpec.describe Merchant, type: :model do
 
   describe 'relationships' do
     it { should have_many :items }
+  end
+  
+  describe 'class and model' do
+   
+    it 'returns a merchant given a search term (case sensitive)' do
+      merch1 = create(:merchant, name: 'Turing')
+      merch2 = create(:merchant, name: 'Ring World')
+      merch3 = create(:merchant, name: 'Turkey Town')
+
+      expect(Merchant.find_one_by_search_term('Ring World')).to eq(merch2)
+    end
+
   end 
 
 end 
