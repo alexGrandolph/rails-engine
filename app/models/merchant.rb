56b0792a44b37bb3f05a45ef2f855323d9f4224e
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-18 11:05:31 -0600

    Feat: add Merchant.find_one_by_search_term method to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,10 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   has_many :items
+
+  def self.find_one_by_search_term(search_term)
+    
+    Merchant.where("name ILIKE ?", "%#{search_term}%").first
+
+  end 
 end 
