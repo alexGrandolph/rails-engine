7761d8b3a5f9add6af5b8c5ab1106e036dcc6ea1
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-18 15:37:55 -0600

    Test: add test for Item.items_above_price method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,6 +31,16 @@ RSpec.describe Item, type: :model do
       item3 = create(:item, name: 'just some cheese', merchant_id: merch.id)
      
       expect(Item.find_all_by_search_term('cheese')).to eq([item2, item4, item3])
+    end 
+
+    it 'returns items equal to or above a given price' do
+      merch = create(:merchant)
+      item1 = create(:item, unit_price: 3.99, merchant_id: merch.id)
+      item2 = create(:item, unit_price: 16.88, merchant_id: merch.id)
+      item3 = create(:item, unit_price: 4.99, merchant_id: merch.id)
+      item4 = create(:item, unit_price: 2.11, merchant_id: merch.id)
+    
+    expect(Item.items_above_price(4.99)).to eq([item2, item3])
 
     end 
     
