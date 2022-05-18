aedf079991dbd42cbc36d78205984001e7638891
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-18 09:58:31 -0600

    Feat: add items/find_all route and #index action in item_search controller, #index action uses to be developed find_all_by_search_term method

--- app/controllers/api/v1/item_search_controller.rb
+++ app/controllers/api/v1/item_search_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::ItemSearchController < ApplicationController
 
+  def index
+    search_term = params[:term]
+    found_items = Item.find_all_by_search_term(search_term)
+    render json: ItemSerializer.new(found_item)
+  end
+  
+
   def show
     search_term = params[:name]
-    # binding.pry
     found_item = Item.find_one_by_search_term(search_term)
     if found_item.nil?
       render json: { data: { message: "No item containing #{search_term} was found" } }
