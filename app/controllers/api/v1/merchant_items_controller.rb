86131b2811b7620140345921ecfc6226be96e1e0
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-17 19:12:43 -0600

    Fix: fix type in merchant_items#index

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::MerchantItemsController < ApplicationController
   def index
     merch_id = params[:merchant_id]    
     if Item.exists?(merchant_id: merch_id)
+      merch = Merchant.find(merch_id)
       render json: ItemSerializer.new(merch.items)
     else
       render status: 404
